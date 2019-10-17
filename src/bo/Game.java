3bcd77adb27faa9e3e7bf54103dd23e39af1c805
RemsNoel <remy.noel@epsi.fr>
2019-10-17 09:10:02 +0200

    Creation class

---
+++ src/bo/Game.java
@@ -0,0 +1,43 @@
+package bo;
+
+import java.util.Date;
+
+public class Game {
+
+    private String pseudo;
+    private String score;
+    private Date date;
+
+    public Game() {
+    }
+
+    public Game(String pseudo, String score, Date date) {
+        this.pseudo = pseudo;
+        this.score = score;
+        this.date = date;
+    }
+
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
---
+++ src/bo/Player.java
@@ -0,0 +1,41 @@
+package bo;
+
+public class Player {
+
+    private String pseudo;
+    private String login;
+    private String password;
+
+    public Player() {
+    }
+
+    public Player(String pseudo, String login, String password) {
+        this.pseudo = pseudo;
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
