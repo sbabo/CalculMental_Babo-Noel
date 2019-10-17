34c6bb63c7aeb063bbd01e7cb63b9d87902bbc00
RemsNoel <remy.noel@epsi.fr>
2019-10-17 11:34:23 +0200

    HighScoreController

---
+++ src/controller/HighScoreController.java
@@ -0,0 +1,24 @@
+package controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet (urlPatterns = "/highscore")
+public class HighScoreController extends HttpServlet {
+
+    public static final String PAGE_HIGH_SCORE_JSP = "/WEB-INF/jsp/high_score.jsp";
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher(PAGE_HIGH_SCORE_JSP).forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
