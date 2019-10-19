21bcc4fbf0021b536a2d0dd1fb224a33982c5157
Samuel <babo.samuel0@gmail.com>
2019-10-19 13:46:29 +0200

    [ADD] Login filter

--- src/filter/LoginFilter.java
+++ src/filter/LoginFilter.java
@@ -1,10 +1,14 @@
 package filter;
 
+import model.LoginBean;
+
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-//@WebFilter (urlPatterns ="/login")
+@WebFilter (urlPatterns ="/highscore")
 public class LoginFilter implements Filter {
 
     @Override
@@ -15,6 +19,12 @@ public class LoginFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         //TODO A completer quand on aura les différentes pages de redirection
+        HttpSession session = ((HttpServletRequest)servletRequest).getSession(  );
+        if ( null == session || null == session.getAttribute( LoginBean.ATT_AUTH_SESSION )) {
+            ((HttpServletRequest)servletRequest).getRequestDispatcher( "login" ).forward( servletRequest, servletResponse );
+        } else {
+            filterChain.doFilter( servletRequest, servletResponse );
+        }
     }
 
     @Override
